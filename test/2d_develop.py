855b18b627baf7c1bae2ac54d2986e6864586b67
umezaki <zodiac.pol.mig@gmail.com>
2019-12-05 12:57:33 +0900

    make ax version

--- test/2d_develop.py
+++ test/2d_develop.py
@@ -1,5 +1,7 @@
 import ctypes
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
+import numpy as np
 
 def st_line(x1, x2, x):
     y=(30/(x2-x1))*(x-x1)-15
@@ -15,7 +17,10 @@ def main():
         xu_list.append(float(data[0]))
         xd_list.append(float(data[1]))
 
-    print(xu_list)
+#    print(xu_list)
+    xu_list=np.array(xu_list)
+    xd_list=np.array(xd_list)
+    print(xu_list, xd_list)
     plt.ion
 
     for i in range(1, 200):
@@ -25,13 +30,20 @@ def main():
       plt.ylim(-20, 20)
       #l1,= plt.plot([xd_list[i], xu_list[i]], [st_line(xd_list[i], xu_list[i], -12), st_line(xd_list[i], xu_list[i], 12)])
       l1,= plt.plot([-10, 10], [st_line(xd_list[i], xu_list[i], -10), st_line(xd_list[i], xu_list[i], 10)])
-      l2,= plt.plot([xd_list[i], xu_list[i]], [-15, 15], color='Red') #for check
+      #l2,= plt.plot([xd_list[i], xu_list[i]], [-15, 15], color='Red') #for check
       plt.draw()
+
+      #axprev = plt.axes([0.7, 0.01, 0.1, 0.04])
+      #axnext = plt.axes([0.81, 0.01, 0.1, 0.04])
+      #bnext = Button(axnext, 'Next')
+      #bprev = Button(axprev, 'Previous')
+
       plt.pause(0.3)
       plt.cla()
       if input()=='q':
           break
     plt.close()
+
 
 if __name__=='__main__':
     main()
